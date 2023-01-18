2c33eabbdbce8d4cac699a3d8646936b484ce6f6
SwampMaster2160 <jaxon1454@gmail.com>
2023-01-18 14:50:03 +1300

    123

--- src/gui/gui_menu.rs
+++ src/gui/gui_menu.rs
@@ -15,225 +15,7 @@ pub struct GUIMenu {
 }
 
 impl GUIMenu {
-	/// Get the elements that do not need to be stored from frame to frame.
-	pub fn get_const_elements(&self, world: &Option<World>) -> Vec<GUIElement> {
-		match &self.variant {
-			GUIMenuVariant::Paused => vec![
-				GUIElement::Rect { rect: GUIRect::new(51, 28, 154, 200), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR },
-				GUIElement::Text { text: "Game Paused".to_string(), pos: [127, 14], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
-				GUIElement::Button {
-					rect: GUIRect::new(53, 30, 150, 16), alignment: GUIAlignment::Center, text: "Resume".to_string(), enabled: true,
-					click_mut_gui: (|_, gui, _, _| {gui.menus.pop();}),
-				},
-				GUIElement::Button {
-					rect: GUIRect::new(53, 210, 150, 16), alignment: GUIAlignment::Center, text: "Exit Game".to_string(), enabled: true,
-					click_mut_gui: (|_, gui, world, _| {
-						if let Some(world) = world {
-							world.is_freeing = true;
-						}
-						gui.menus = Vec::new();
-						gui.menus.push(Self::new(GUIMenuVariant::ExitingGame));
-					}),
-				},
-				GUIElement::Button {
-					rect: GUIRect::new(53, 190, 150, 16), alignment: GUIAlignment::Center, text: "Exit to Title".to_string(), enabled: true,
-					click_mut_gui: (|_, gui, world, _| {
-						if let Some(world) = world {
-							world.is_freeing = true;
-						}
-						gui.menus = Vec::new();
-						gui.menus.push(Self::new(GUIMenuVariant::ExitingToTitle));
-					}),
-				},
-			],
-			GUIMenuVariant::ExitingGame => vec![
-				GUIElement::Rect { rect: GUIRect::new(51, 76, 154, 104), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR },
-				GUIElement::Text { text: "Closing World...".to_string(), pos: [127, 120], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
-			],
-			GUIMenuVariant::ExitingToTitle => vec![
-				GUIElement::Rect { rect: GUIRect::new(51, 76, 154, 104), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR },
-				GUIElement::Text { text: "Closing World...".to_string(), pos: [127, 120], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
-			],
-			
-			GUIMenuVariant::IngameHUD => {
-				let mut out = Vec::new();
-				let world = world.as_ref().unwrap();
-				let player = world.player.as_ref().unwrap();
-				let (inventory, selected_item, is_swaping_items) = match &player.entity_type {
-					EntityType::Player { inventory, selected_item, is_swaping_item, .. } => (inventory, selected_item, is_swaping_item),
-				};
-				// Item area
-				let mut grid_elements = Vec::new();
-				for (item_index, item_stack) in inventory.items.iter().enumerate() {
-					let mut cell_elements = Vec::new();
-					let x = item_index as u16 % 10;
-					let y = item_index as u16 / 10;
-					// Add cell gray rect.
-					let color = match (x % 2 == 0) ^ (y % 2 == 0)  {
-						true => [63, 63, 63, 63],
-						false => [31, 31, 31, 63],
-					};
-					cell_elements.push(GUIElement::Rect { rect: GUIRect::new(0, 0, 16, 16), alignment: GUIAlignment::Left, inside_color: NO_COLOR, border_color: color });
-					// Item texture
-					let stack_size = item_stack.1;
-					if stack_size > 0 {
-						cell_elements.push(GUIElement::Texture { pos: [0, 0], alignment: GUIAlignment::Left, texture: item_stack.0.get_texture() });
-					}
-					// The text to show how many items are in a stack.
-					if stack_size > 1 {
-						cell_elements.push(GUIElement::Text { pos: [0, -4], alignment: GUIAlignment::Left, text: stack_size.to_string(), text_alignment: GUIAlignment::Left });
-					}
-					// Show selected item.
-					if item_index == *selected_item as usize {
-						let color = match *is_swaping_items {
-							false => [63, 63, 63, 127],
-							true => [31, 31, 31, 127],
-						};
-						cell_elements.push(GUIElement::Rect { rect: GUIRect::new(0, 0, 16, 16), alignment: GUIAlignment::Left, inside_color: NO_COLOR, border_color: color });
-					}
-
-					grid_elements.push(GUIElement::ElementCollection { offset: [0, 0], inside_elements: cell_elements })
-				}
-				out.push(GUIElement::Grid { alignment: GUIAlignment::Left , cell_rect: GUIRect::new(0, 0, 16, 16), cell_counts: [10, 5], inside_elements: grid_elements, click_mut_gui: |_, _, world, _, item_clicked_on_index|{
-					let world = match world {
-						Some(world) => world,
-						_ => return,
-					};
-					let player = match &mut world.player {
-						Some(player) => player,
-						_ => return,
-					};
-					let (selected_item, is_swaping_item, inventory) = match &mut player.entity_type {
-						EntityType::Player { selected_item, is_swaping_item, inventory, .. } => (selected_item, is_swaping_item, inventory),
-						//_ => return,
-					};
-					if *selected_item == item_clicked_on_index as u8 {
-						*is_swaping_item = !*is_swaping_item;
-					}
-					else {
-						if *is_swaping_item {
-							inventory.swap_items(*selected_item as usize, item_clicked_on_index);
-						}
-						*is_swaping_item = false;
-						*selected_item = item_clicked_on_index as u8;
-					}
-				} });
-				// Health bar
-				if world.difficulty != Difficulty::Sandbox {
-					out.push(GUIElement::ProgressBar {
-						rect: GUIRect::new(256 - 202 - 2, 2, 202, 8), alignment: GUIAlignment::Right, inside_color: [255, 0, 0, 255], border_color: [0, 0, 0, 255],
-						progress: player.health, max_progress: EntityVariant::Player.max_health(),
-					});
-				}
-				out
-			}
-			GUIMenuVariant::Error => vec![
-				GUIElement::Grayout { color: GRAYOUT_COLOR },
-				GUIElement::Rect { rect: GUIRect::new(51, 88, 154, 80), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR },
-				GUIElement::Text { text: "Error".to_string(), pos: [127, 74], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
-				GUIElement::Button {
-					rect: GUIRect::new(53, 150, 150, 16), alignment: GUIAlignment::Center, text: "OK".to_string(), enabled: true,
-					click_mut_gui: (|_, gui, _, _| {
-						gui.menus.pop();
-					}),
-				},
-			],
-			_ => Vec::new(),
-		}
-	}
-
-	/// Get all elements.
-	pub fn get_elements(&self, world: &Option<World>) -> Vec<GUIElement> {
-		let mut out = self.get_const_elements(world);
-		out.extend(self.extra_elements.clone());
-		out
-	}
-
-	/// Render all elements.
-	pub fn render(&self, vertices: &mut Vec<Vertex>, io: &IO, world: &Option<World>) {
-		for element in self.get_elements(world) {
-			element.render(GUIRect::EVERYTHING, vertices, io, [0, 0]);
-		}
-	}
-
-	/// Weather the game should pause when this menu is in the GUI stack.
-	pub fn does_menu_pause_game(&self) -> bool {
-		match self.variant {
-			GUIMenuVariant::Test => true,
-			GUIMenuVariant::Paused => true,
-			GUIMenuVariant::ExitingGame => true,
-			GUIMenuVariant::ExitingToTitle => true,
-			GUIMenuVariant::Title => true,
-			GUIMenuVariant::IngameHUD => false,
-			GUIMenuVariant::CreateWorld => true,
-			GUIMenuVariant::Error => true,
-			GUIMenuVariant::LoadWorld { .. } => true,
-		}
-	}
-
-	/// What to do when Esc is pressed.
-	pub fn menu_close_button_action(self, gui: &mut GUI, _world: &mut Option<World>, io: &mut IO) {
-		match self.variant {
-			GUIMenuVariant::Paused => {
-				gui.menus.pop();
-				io.update_keys_pressed_last();
-			},
-			GUIMenuVariant::Test => {
-				gui.menus.pop();
-				io.update_keys_pressed_last();
-			},
-			_ => {}
-		}
-	}
-
-	/// Menu tick.
-	pub fn tick(self, gui: &mut GUI, world: &mut Option<World>, io: &mut IO, request_game_close: bool) {
-		if request_game_close {
-			if world.is_some() {
-				if let Some(world) = world {
-					world.is_freeing = true;
-				}
-				gui.menus = Vec::new();
-				gui.menus.push(Self::new(GUIMenuVariant::ExitingGame));
-			}
-			else if matches!(self.variant, GUIMenuVariant::ExitingGame) || matches!(self.variant, GUIMenuVariant::ExitingToTitle) {
-
-			}
-			else {
-				gui.should_close_game = true;
-			}
-		}
-		match self.variant {
-			GUIMenuVariant::IngameHUD => {
-				if io.get_game_key_starting_now(GameKey::OpenTestMenu) {
-					gui.menus.push(Self::new(GUIMenuVariant::Test))
-				}
-			}
-			GUIMenuVariant::ExitingGame => {
-				if let Some(world) = world {
-					if world.is_freed {
-						gui.should_close_game = true;
-					}
-				}
-			}
-			GUIMenuVariant::ExitingToTitle => {
-				let mut set_world_to_none = false;
-				if let Some(world) = world {
-					if world.is_freed {
-						gui.menus.pop();
-						gui.menus.push(Self::new(GUIMenuVariant::Title));
-						set_world_to_none = true;
-					}
-				}
-				if set_world_to_none {
-					*world = None;
-				}
-			}
-			_ => {}
-		}
-	}
-
-	/// Create the menu setting all the elements that need to be stored between frames.
+	/// Create the menu with all the elements that will to be stored between frames.
 	pub fn new(variant: GUIMenuVariant) -> Self {
 		Self {
 			variant: variant.clone(),
@@ -355,7 +137,7 @@ impl GUIMenu {
 					},
 				],
 				GUIMenuVariant::Title => vec![
-					GUIElement::RectContainer { 
+					GUIElement::RectContainer {
 						rect: GUIRect::new(51, 28, 154, 200), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR, inside_elements: vec![
 							GUIElement::Text { text: "Ceolmund".to_string(), pos: [77, -20], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
 							GUIElement::Button {
@@ -382,14 +164,14 @@ impl GUIMenu {
 					},
 				],
 				GUIMenuVariant::LoadWorld { world_list: load_world_data } => {
+					// Create world buttons
 					let world_count = load_world_data.worlds.len();
-				
 					let mut buttons = Vec::new();
 					for world_index in 0..world_count {
 						let world = load_world_data.worlds[world_index].clone();
 						buttons.push((world.0, GUIRect::new(0, world_index as i16 * 20, 146, 16), true));
 					}
-
+					// Create the button group for the buttons with one function.
 					let buttons = GUIElement::SingleFunctionButtonGroup {
 						alignment: GUIAlignment::Center, buttons,
 						click_mut_gui: (|_, gui, world, io, button_clicked_index| {
@@ -406,9 +188,9 @@ impl GUIMenu {
 							}
 						}),
 					};
-					
+					// Vector with all elements.
 					vec![
-						GUIElement::RectContainer { 
+						GUIElement::RectContainer {
 							rect: GUIRect::new(51, 28, 154, 200), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR, inside_elements: vec![
 								GUIElement::Text { text: "Load World".to_string(), pos: [77, -20], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
 								GUIElement::ScrollArea {
@@ -427,18 +209,237 @@ impl GUIMenu {
 						},
 					]
 				}
-				_ => Vec::new(),
+				GUIMenuVariant::Paused => vec![
+					GUIElement::RectContainer {
+						rect: GUIRect::new(51, 28, 154, 200), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR, inside_elements: vec![
+							GUIElement::Text { text: "Game Paused".to_string(), pos: [77, -20], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
+							GUIElement::Button {
+								rect: GUIRect::new(0, 0, 150, 16), alignment: GUIAlignment::Center, text: "Resume".to_string(), enabled: true,
+								click_mut_gui: (|_, gui, _, _| {gui.menus.pop();}),
+							},
+							GUIElement::Button {
+								rect: GUIRect::new(0, 160, 150, 16), alignment: GUIAlignment::Center, text: "Exit to Title".to_string(), enabled: true,
+								click_mut_gui: (|_, gui, world, _| {
+									if let Some(world) = world {
+										world.is_freeing = true;
+									}
+									gui.menus = Vec::new();
+									gui.menus.push(Self::new(GUIMenuVariant::ExitingToTitle));
+								}),
+							},
+							GUIElement::Button {
+								rect: GUIRect::new(0, 180, 150, 16), alignment: GUIAlignment::Center, text: "Exit Game".to_string(), enabled: true,
+								click_mut_gui: (|_, gui, world, _| {
+									if let Some(world) = world {
+										world.is_freeing = true;
+									}
+									gui.menus = Vec::new();
+									gui.menus.push(Self::new(GUIMenuVariant::ExitingGame));
+								}),
+							},
+						],
+					},
+				],
+				GUIMenuVariant::ExitingGame | GUIMenuVariant::ExitingToTitle => vec![
+					GUIElement::RectContainer {
+						rect: GUIRect::new(51, 76, 154, 104), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR, inside_elements: vec![
+							GUIElement::Text { text: "Closing World...".to_string(), pos: [77, 44], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
+						],
+					},
+				],
+				GUIMenuVariant::Error => vec![
+					GUIElement::Grayout { color: GRAYOUT_COLOR },
+					GUIElement::RectContainer {
+						rect: GUIRect::new(51, 88, 154, 80), alignment: GUIAlignment::Center, inside_color: RECT_COLOR, border_color: RECT_BORDER_COLOR, inside_elements: vec![
+							GUIElement::Text { text: "Error".to_string(), pos: [77, -20], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
+							GUIElement::Button {
+								rect: GUIRect::new(0, 60, 150, 16), alignment: GUIAlignment::Center, text: "OK".to_string(), enabled: true,
+								click_mut_gui: (|_, gui, _, _| {
+									gui.menus.pop();
+								}),
+							},
+						],
+					},
+				],
+				GUIMenuVariant::IngameHUD => Vec::new(),
 			},
+		}
+	}
+
+	/// Get the elements that must be created each frame.
+	pub fn get_frame_elements(&self, world: &Option<World>) -> Vec<GUIElement> {
+		match &self.variant {
+			GUIMenuVariant::IngameHUD => {
+				let mut out = Vec::new();
+				let world = world.as_ref().unwrap();
+				let player = world.player.as_ref().unwrap();
+				let (inventory, selected_item, is_swaping_items) = match &player.entity_type {
+					EntityType::Player { inventory, selected_item, is_swaping_item, .. } => (inventory, selected_item, is_swaping_item),
+				};
+				// Item area
+				let mut grid_elements = Vec::new();
+				for (item_index, item_stack) in inventory.items.iter().enumerate() {
+					let mut cell_elements = Vec::new();
+					let x = item_index as u16 % 10;
+					let y = item_index as u16 / 10;
+					// Add cell gray rect.
+					let color = match (x % 2 == 0) ^ (y % 2 == 0)  {
+						true => [63, 63, 63, 63],
+						false => [31, 31, 31, 63],
+					};
+					cell_elements.push(GUIElement::Rect { rect: GUIRect::new(0, 0, 16, 16), alignment: GUIAlignment::Left, inside_color: NO_COLOR, border_color: color });
+					// Item texture
+					let stack_size = item_stack.1;
+					if stack_size > 0 {
+						cell_elements.push(GUIElement::Texture { pos: [0, 0], alignment: GUIAlignment::Left, texture: item_stack.0.get_texture() });
+					}
+					// The text to show how many items are in a stack.
+					if stack_size > 1 {
+						cell_elements.push(GUIElement::Text { pos: [0, -4], alignment: GUIAlignment::Left, text: stack_size.to_string(), text_alignment: GUIAlignment::Left });
+					}
+					// Show selected item.
+					if item_index == *selected_item as usize {
+						let color = match *is_swaping_items {
+							false => [63, 63, 63, 127],
+							true => [31, 31, 31, 127],
+						};
+						cell_elements.push(GUIElement::Rect { rect: GUIRect::new(0, 0, 16, 16), alignment: GUIAlignment::Left, inside_color: NO_COLOR, border_color: color });
+					}
+
+					grid_elements.push(GUIElement::ElementCollection { offset: [0, 0], inside_elements: cell_elements })
+				}
+				out.push(GUIElement::Grid { alignment: GUIAlignment::Left , cell_rect: GUIRect::new(0, 0, 16, 16), cell_counts: [10, 5], inside_elements: grid_elements, click_mut_gui: |_, _, world, _, item_clicked_on_index|{
+					let world = match world {
+						Some(world) => world,
+						_ => return,
+					};
+					let player = match &mut world.player {
+						Some(player) => player,
+						_ => return,
+					};
+					let (selected_item, is_swaping_item, inventory) = match &mut player.entity_type {
+						EntityType::Player { selected_item, is_swaping_item, inventory, .. } => (selected_item, is_swaping_item, inventory),
+						//_ => return,
+					};
+					if *selected_item == item_clicked_on_index as u8 {
+						*is_swaping_item = !*is_swaping_item;
+					}
+					else {
+						if *is_swaping_item {
+							inventory.swap_items(*selected_item as usize, item_clicked_on_index);
+						}
+						*is_swaping_item = false;
+						*selected_item = item_clicked_on_index as u8;
+					}
+				} });
+				// Health bar
+				if world.difficulty != Difficulty::Sandbox {
+					out.push(GUIElement::ProgressBar {
+						rect: GUIRect::new(256 - 202 - 2, 2, 202, 8), alignment: GUIAlignment::Right, inside_color: [255, 0, 0, 255], border_color: [0, 0, 0, 255],
+						progress: player.health, max_progress: EntityVariant::Player.max_health(),
+					});
+				}
+				out
+			}
+			_ => Vec::new(),
+		}
+	}
+
+	/// Get all elements.
+	pub fn get_elements(&self, world: &Option<World>) -> Vec<GUIElement> {
+		let mut out = self.get_frame_elements(world);
+		out.extend(self.extra_elements.clone());
+		out
+	}
+
+	/// Render all elements.
+	pub fn render(&self, vertices: &mut Vec<Vertex>, io: &IO, world: &Option<World>) {
+		for element in self.get_elements(world) {
+			element.render(GUIRect::EVERYTHING, vertices, io, [0, 0]);
+		}
+	}
+
+	/// Weather the game should pause when this menu is in the GUI stack.
+	pub fn does_menu_pause_game(&self) -> bool {
+		match self.variant {
+			GUIMenuVariant::Test => true,
+			GUIMenuVariant::Paused => true,
+			GUIMenuVariant::ExitingGame => true,
+			GUIMenuVariant::ExitingToTitle => true,
+			GUIMenuVariant::Title => true,
+			GUIMenuVariant::IngameHUD => false,
+			GUIMenuVariant::CreateWorld => true,
+			GUIMenuVariant::Error => true,
+			GUIMenuVariant::LoadWorld { .. } => true,
+		}
+	}
+
+	/// What to do when Esc is pressed.
+	pub fn menu_close_button_action(self, gui: &mut GUI, _world: &mut Option<World>, io: &mut IO) {
+		match self.variant {
+			GUIMenuVariant::Paused => {
+				gui.menus.pop();
+				io.update_keys_pressed_last();
+			},
+			GUIMenuVariant::Test => {
+				gui.menus.pop();
+				io.update_keys_pressed_last();
+			},
+			_ => {}
+		}
+	}
+
+	/// Menu tick.
+	pub fn tick(self, gui: &mut GUI, world: &mut Option<World>, io: &mut IO, request_game_close: bool) {
+		if request_game_close {
+			if world.is_some() {
+				if let Some(world) = world {
+					world.is_freeing = true;
+				}
+				gui.menus = Vec::new();
+				gui.menus.push(Self::new(GUIMenuVariant::ExitingGame));
+			}
+			else if matches!(self.variant, GUIMenuVariant::ExitingGame) || matches!(self.variant, GUIMenuVariant::ExitingToTitle) {
+
+			}
+			else {
+				gui.should_close_game = true;
+			}
+		}
+		match self.variant {
+			GUIMenuVariant::IngameHUD => {
+				if io.get_game_key_starting_now(GameKey::OpenTestMenu) {
+					gui.menus.push(Self::new(GUIMenuVariant::Test))
+				}
+			}
+			GUIMenuVariant::ExitingGame => {
+				if let Some(world) = world {
+					if world.is_freed {
+						gui.should_close_game = true;
+					}
+				}
+			}
+			GUIMenuVariant::ExitingToTitle => {
+				let mut set_world_to_none = false;
+				if let Some(world) = world {
+					if world.is_freed {
+						gui.menus.pop();
+						gui.menus.push(Self::new(GUIMenuVariant::Title));
+						set_world_to_none = true;
+					}
+				}
+				if set_world_to_none {
+					*world = None;
+				}
+			}
+			_ => {}
 		}
 	}
 
 	/// Create a error GUI menu from a string.
 	pub fn new_error(error: String) -> Self {
-		Self {
-			variant: GUIMenuVariant::Error,
-			extra_elements: vec![
-				GUIElement::Text { text: error, pos: [127, 116], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center },
-			],
-		}
+		let mut out = Self::new(GUIMenuVariant::Error);
+		out.extra_elements.push(GUIElement::Text { text: error, pos: [127, 116], alignment: GUIAlignment::Center, text_alignment: GUIAlignment::Center });
+		out
 	}
 }
