c72fd09c522cefc27b8bc43e15fefcb43abed273
SwampMaster2160 <jaxon1454@gmail.com>
2023-01-21 15:47:04 +1300

    131

---
+++ src/world/item/crafting_recipes.rs
@@ -0,0 +1,24 @@
+use super::item::Item;
+
+macro_rules! recipe {
+	( $in:expr, $out:expr ) => {
+		{
+			const A: &[(Item, u16)] = $in.as_slice();
+			const B: &[(Item, u16)] = $out.as_slice();
+			(A, B)
+		}
+	};
+}
+
+#[derive(Copy, Clone)]
+pub enum CraftingRecipes {
+	QUICK,
+}
+
+impl CraftingRecipes {
+	pub const fn get_recipes(self) -> &'static [(&'static [(Item, u16)], &'static [(Item, u16)])] {
+		match self {
+			Self::QUICK => [recipe!([(Item::SandboxDestroyWand, 2)], [(Item::SandboxDestroyWand, 1)])].as_slice(),
+		}
+	}
+}
--- src/world/item/mod.rs
+++ src/world/item/mod.rs
@@ -1,3 +1,4 @@
 pub mod item;
 pub mod item_drop;
 pub mod inventory;
+pub mod crafting_recipes;
